0a49ade5b52d6367f3087963aa439c1a014ea206
李剑飞 <lijianfei@sishuguojixuefu.com>
2018-06-29 17:56:59 +0800

    learn

---
+++ Day02/var1.py
@@ -0,0 +1,10 @@
+a = 321
+b = 123
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b) # 整除
+print(a ** b) # 指数
+
+##################################
---
+++ Day02/var2.py
@@ -0,0 +1,9 @@
+a = int(input('a = ')) # input函数输入
+b = int(input('b = '))
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
